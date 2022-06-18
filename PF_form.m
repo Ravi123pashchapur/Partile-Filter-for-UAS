18b857277263c95c88eb585278ae234327a3b4b9
Ravi Ashok Pashchapur <56997905+Ravi123pashchapur@users.noreply.github.com>
2022-06-18 12:43:58 +0100

    Add files via upload

---
+++ PF_form.m
@@ -0,0 +1,88 @@
+
+function [x_state,P_cov,K_EKF_gain,diff,Z_hat]=PF_form(xy1,xy2,h_0,alpha,x_state_ini,P_cov_ini,F,G,Q,R)
+
+    
+    persistent Part X_s P_s init
+    
+ %% Initilization of estimate and error covarience.
+    
+    N_particle = 1000;                                                        % Number of Particles
+    
+    if isempty(init)
+        init = 1;
+        
+        for i = 1 : N_particle                                               % Initialization of particle to both variances
+            
+            P_init(1,:)=4000*rand(1,N_particle) + 4000;                     
+            P_init(2,:)=4000*rand(1,N_particle) + 4000;            
+        end
+        
+        
+        Part = P_init;                                                         % Initial Particle covariance
+        
+    end
+    
+    
+ %% Convolution of Particle Filter 
+    
+    Z_p = alpha;                                                             % Jammer Measurment
+    
+    for i = 1 : N_particle
+        P_o(:, i) = Part(:, i) + G*sqrt(Q) * [randn; randn];
+        Z_hat = measure(xy1,xy2,P_o(:, i),h_0)+ sqrt(R) * randn;             % Measurment of Particle
+        diff =  Z_p - Z_hat;                                                 % Difference between Jammer mesurement and estimated measurement
+        W(i) = (1 / sqrt(R) / sqrt(2 * pi)) * exp(-(diff)^2 / 2 / R);        % Weights of the Particle
+    end
+
+ % Measurement Update
+    W_sum = sum(W);                                                          % Summation of Weights
+    
+    for i = 1 : N_particle
+        W(i) = W(i) / W_sum;                                                 % Normalization of individual weights
+    end
+    
+ %% Resampling step
+    
+    constant_1 = length(W);
+    i =1;
+    constant_2 = rand/N_particle;
+    j = 0;
+    while j < constant_1
+        j = j + 1;
+        Ns = floor(N_particle*(W(j)-constant_2))+1;
+          constant_3 = 1;
+          while constant_3 <= Ns
+            Part(:,i)=P_o(:,j); 
+            i = i + 1; 
+            constant_3 = constant_3 + 1;
+          end
+        constant_2 = constant_2 + Ns/N_particle-W(j);
+    end 
+    
+    
+ %% Plotting of particles in figure
+    
+ hold on
+ particles = plot(Part(1,:)/10^3,Part(2,:)/10^3,'b*');
+ pause(0.01)
+ delete(particles)
+ 
+ % Mean of the particle
+ X_s = mean(Part');
+
+
+ %% Update Step for State Estimate and Error Covarience.
+ 
+ x_state = X_s;                                                             % Updated State Estimate
+ 
+ P_s = eye(2);
+ 
+ K_EKF_gain = [0; 0];
+ 
+ P_cov=P_s;                                                                 % Updated Error Covarience
+
+end 
+
+
+
+
---
+++ Result.m
@@ -0,0 +1,42 @@
+clc
+clear all;
+close all;
+% Load Mat file
+load result.mat
+
+% Plot the true and predicted jammer state 'x' and 'y'
+figure
+yline(x_t_vec(1),'r--');
+hold on
+plot(x_state(1,:),'r');
+yline(x_t_vec(2),'b--');
+plot(x_state(2,:),'b');
+xlim([50 1800])
+legend('x True position','x Estimate position','y True position','y Estimate position')
+xlabel('Time step')
+ylabel('Jammer position [m]')
+title('True and Predicted Jammer State x and y')
+
+% Plot of True and Estimate Measurement 
+figure
+plot(P_r_filt_ratio(:,1),'r');
+hold on
+plot(h(1,:),'b--');
+xlim([50 1800])
+legend('True Measurement','Estimate Measurement')
+xlabel('Time step')
+ylabel('Measurement')
+title('True and Predicted Measurement')
+
+%Plot Innovation
+figure
+plot(Innovation(1,:))
+hold on
+yline(0)
+xlim([50 1800])
+legend('UKF Innovation')
+xlabel('Time step')
+ylabel('Innovation')
+title('Innovation for Particle Filter')
+
+
